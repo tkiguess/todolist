0863b06c2ca4d8008ef30aef41b2060908c6d172
tkiguess <ahmedtafkir@gmail.com>
2018-08-16 16:50:45 -0400

    fully working

--- src/main/java/com/example/todolist/HomeController.java
+++ src/main/java/com/example/todolist/HomeController.java
@@ -2,13 +2,12 @@ package com.example.todolist;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -18,6 +17,11 @@ import java.security.Principal;
 public class HomeController {
     @Autowired
     private UserService userService;
+    
+    @Autowired
+     TodoRepository todoRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping(value="/register", method= RequestMethod.GET)
     public String showRegistrationPage(Model model){
@@ -41,7 +45,40 @@ public class HomeController {
         return "index";
     }
 
+    @RequestMapping("/update/{id}")
+    public String userUpdate(@PathVariable("id") long id, Model model) {
+        model.addAttribute("todo", todoRepository.findById(id).get());
+        return "add";
+    }
+    @RequestMapping("/delete/{id}")
+    public String delCourse(@PathVariable("id") long id){
+        todoRepository.deleteById(id);
+        return "redirect:/todolist";
+    }
 
+
+    @GetMapping("/add")
+    public String addTodo(Model model){
+        model.addAttribute("todo", new Todo());
+        return "add";
+    }
+    @PostMapping("/process2")
+    public String processForm( @ModelAttribute Todo todo, BindingResult result, Model model)
+    {
+        String username = getUser().getUsername();
+        todo.setUsername(username);
+        todoRepository.save(todo);
+        model.addAttribute("todos", todoRepository.findByUsername(username));
+        return "todolist";
+    }
+
+
+    private User getUser(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentusername = authentication.getName();
+        User user = userRepository.findByUsername(currentusername);
+        return user;
+    }
     @RequestMapping("/")
     public  String index(){
         return "index";
@@ -53,13 +90,14 @@ public class HomeController {
     }
 
     @RequestMapping("/todolist")
-    public String todolist(HttpServletRequest request, Authentication authentication, Principal principal){
+    public String todolist(HttpServletRequest request, Authentication authentication, Principal principal, Model model){
         Boolean isAdmin = request.isUserInRole("ADMIN");
         Boolean isUser = request.isUserInRole("USER");
         UserDetails userDetails = (UserDetails)
                 authentication.getPrincipal();
 
         String username = principal.getName();
+        model.addAttribute("todos", todoRepository.findByUsername(username));
         return "todolist";
     }
 }
